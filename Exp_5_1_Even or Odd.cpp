b18ea5665191923816da1f3561424c5d8ebce935
Papri <papri.sen2020@gmail.com>
2025-07-22 22:57:17 +0530

    Update Exp_5_1_Even or Odd.cpp

--- Exp_5_1_Even or Odd.cpp
+++ Exp_5_1_Even or Odd.cpp
@@ -12,11 +12,20 @@ int main() {
         cout<<"The number is odd."<<endl;
     }
     else if(a=0){
-        cout<<"the number is zero"<<endl;
+        cout<<"the number is zero."<<endl;
     }
     else{
-        cout<<"The number is even"<<endl;
+        cout<<"The number is even."<<endl;
     }
 
     return 0;
 }
+
+/*
+output:
+Enter the number to check even or odd: 2
+The number is even.
+
+Enter the number to check even or odd: 9
+The number is odd.
+*/
