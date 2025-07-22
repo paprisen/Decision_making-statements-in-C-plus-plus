1b9ef8d1e7e40fc9cdc60268c8244c8c52c89dbf
Papri <papri.sen2020@gmail.com>
2025-07-22 23:00:42 +0530

    Update Exp_5_2_Vowel or Consonant.cpp

--- Exp_5_2_Vowel or Consonant.cpp
+++ Exp_5_2_Vowel or Consonant.cpp
@@ -6,18 +6,27 @@ using namespace std;
 
 int main(){
   char ch;
-  cout<<"enter the character = ";
+  cout<<"Enter the character = ";
   cin>>ch;
   if(ch=='a'||ch=='A'||ch=='e' ||ch=='E'||ch=='i'||ch=='I'||ch=='o' ||ch=='O' || ch=='u' || ch=='U' )
   {
-      cout<<"the character is vowel ";
+      cout<<"The character is vowel ";
   }
   else
   {
-      cout<<"the character is consonant";
+      cout<<"The character is consonant";
   }
   
     
     
     return 0;
 }
+
+/*
+output:
+Enter the character = A
+The character is vowel 
+
+Enter the character = P
+The character is consonant
+*/
