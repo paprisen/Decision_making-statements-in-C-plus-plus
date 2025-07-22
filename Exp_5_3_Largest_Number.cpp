c01fa4d61c7d501e8a5529bf40be4d7f1aff9dc6
Papri <papri.sen2020@gmail.com>
2025-07-22 23:03:28 +0530

    Update Exp_5_3_Largest_Number.cpp

--- Exp_5_3_Largest_Number.cpp
+++ Exp_5_3_Largest_Number.cpp
@@ -3,33 +3,47 @@
 //A3
 #include <iostream>
 using namespace std;
+int main()
+{
+  int a,b,c;
+  cout << "Enter Number 1:";
+  cin >> a;
+  cout << "Enter Number 2:";
+  cin >> b;
+  cout << "Enter Number 3:";
+  cin >> c;
+  if(a>b){
+      if(a>c){
+           cout << "Number 1: " << a << " is largest" << endl;
+      }
+  }
 
-int main() {
-    int n1, n2, n3;
-    cout << "Enter 1st number: ";
-    cin>>n1;
-    cout << "Enter 2nd number: ";
-    cin>>n2;
-    cout << "Enter 3rd number: ";
-    cin>>n3;
-    if(n1>n2){
-        if(n1>n3){
-            cout<<n1<<" is the largest"<<endl;
-        }
-        else{
-            cout<<n3<<" is the largest"<<endl;
-        }
-    }
-    else if(n2>n1){
-        if(n2>n3){
-            cout<<n2<<" is the largest"<<endl;
-        }
-        else{
-            cout<<n3<<" is the largest"<<endl;
-        }
-    }
-    else{
-        cout<<"All numbers are same"<<endl;
-    }
-    return 0;
+  if(b>c){
+      if(b>a){
+           cout << "Number 2: " << b << " is largest" << endl;
+      }
+  }
+    if(c>b){
+      if(c>a){
+           cout << "Number 3: " << c << " is largest" << endl;
+      }
+  }
 }
+  
+/*
+output:
+Enter Number 1:45
+Enter Number 2:33
+Enter Number 3:27
+Number 1: 45 is largest
+
+Enter Number 1:22
+Enter Number 2:66
+Enter Number 3:11
+Number 2: 66 is largest
+
+Enter Number 1:14
+Enter Number 2:19
+Enter Number 3:25
+Number 3: 25 is largest
+*/  
